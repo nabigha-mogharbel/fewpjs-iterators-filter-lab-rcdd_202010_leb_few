a9a58734ae98effbceffba77bf3490b166b759fb
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-01 14:53:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here
 const findMatching= (array, driver) => array.filter(string => string.toLowerCase()===driver.toLowerCase())
 const fuzzyMatch=(array,driver) => array.filter(string => string.charAt(0)===driver.charAt(0))
-const matchName=(drivers,string) => s === array.name)
+const matchName=(drivers,string) => drivers.filter(driver => driver.name === string)
